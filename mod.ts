ffe5b51cce91d1bfe53df506bf633ff14eeaf4a5
abdfnx <abdfnabd@gmail.com>
2022-06-20 14:09:05 +0300

    add the main file of botway telegram deno template

---
+++ mod.ts
@@ -0,0 +1,18 @@
+import { Bot } from "https://deno.land/x/grammy/mod.ts";
+import { getToken } from "https://deno.land/x/denobot/mod.ts";
+
+// Create bot object
+const bot = new Bot(getToken());
+
+// Listen for messages
+bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
+bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
+bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
+bot.on("edited_message", (ctx) =>
+  ctx.reply("Ha! Gotcha! You just edited this!", {
+    reply_to_message_id: ctx.editedMessage.message_id,
+  })
+);
+
+// Launch!
+bot.start();
